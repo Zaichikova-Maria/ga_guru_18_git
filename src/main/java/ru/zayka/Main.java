6be258b3d90892f591a0537f45e879f70cb6fa63
Zaichikova-Maria <zaychikovamaria88@gmail.com>
2026-02-09 21:38:40 +0300

    Update Main.java

--- src/main/java/ru/zayka/Main.java
+++ src/main/java/ru/zayka/Main.java
@@ -1,8 +1,8 @@
 package ru.zayka;
 
 public class Main {
+    private static String msg=""Hello qa.guru!!!!!!"";
     public static void main(String[] args) {
-        System.out.printf("Hello qa.guru!!!!!!");
-
+        System.out.printf("msg");
     }
 }
